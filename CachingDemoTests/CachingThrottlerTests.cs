28fb15dbda7bf2e80a0eb5002409fd7a62c9c751
Piotr Milczarek <piomil@outlook.com>
2018-11-20 16:25:51 +0100

    More test cases

--- CachingDemoTests/CachingThrottlerTests.cs
+++ CachingDemoTests/CachingThrottlerTests.cs
@@ -11,7 +11,10 @@ namespace CachingDemoTests
         [InlineData(3, 100, 0, 0, 1, 2, 3, 4, 0, 1, 2, 2, 2)]
         [InlineData(3, 3,   0, 0, 1, 2, 3, 4, 0, 1, 2, 2, 2)]
         [InlineData(3, 1,   0, 0, 1, 2, 3, 4, 0, 1, 2, 2, 2)]
-        public void CanThrottleWhenCountExceeeded(
+        [InlineData(3, 0,   0, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4)]
+        [InlineData(0, 0,   0, 0, 1, 2, 3, 4, 0, 0, 0, 0, 0)]
+        [InlineData(1, 0,   0, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4)]
+        public void CanThrottle(
             int maxElem, int  timeWindowSeconds, int delayBetweenInMs,
             int in1,  int in2,  int in3,  int in4,  int in5,
             int out1, int out2, int out3, int out4, int out5)
